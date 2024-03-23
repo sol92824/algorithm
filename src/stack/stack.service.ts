cfbbe6acf816a07f53789e090846bc4fa9141002
sol92824 <sol92824@gmail.com>
2024-03-23 20:44:09 +0900

    일부 변수 let > const 변경

--- src/stack/stack.service.ts
+++ src/stack/stack.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class StackService {
   stackMadeWithArray() {
-    let stack = [];
+    const stack = [];
 
     // 입력값을 push로 배열의 마지막에 추가
-    stack.push("google");
-    stack.push("instagram");
-    stack.push("youtube");
+    stack.push('google');
+    stack.push('instagram');
+    stack.push('youtube');
 
     console.log(stack);
 
@@ -16,16 +16,16 @@ export class StackService {
     console.log(stack.pop());
     console.log(stack.pop());
 
-    stack.push("amazon");
+    stack.push('amazon');
 
     console.log(stack.pop());
 
-    let stack2 = [];
+    const stack2 = [];
 
     // 입력값을 unshift로 배열의 처음에 추가
-    stack2.unshift("create new file");
-    stack2.unshift("resized file");
-    stack2.unshift("cloned out wrinkle");
+    stack2.unshift('create new file');
+    stack2.unshift('resized file');
+    stack2.unshift('cloned out wrinkle');
 
     console.log(stack2);
 
@@ -48,18 +48,17 @@ class Stack {
   }
 
   // 1. value를 인자로 받는 push 함수 생성
-  push(value: any):number {
+  push(value: any): number {
     // 2. value로 새로운 Node 생성
-    let newNode = new Node(value);
+    const newNode = new Node(value);
 
     // 3. Stack이 Node를 가지고 있지 않으면, first와 last 속성을 새로 생성된 Node로 설정
     if (this.size === 0) {
       this.first = newNode;
       this.last = newNode;
-    }
-    else {
+    } else {
       // 4. Stack에 Node가 하나라도 있다면, first 속성을 저장할 변수 생성
-      let currentFirst = this.first;
+      const currentFirst = this.first;
 
       // 5. first 속성을 새로 생성된 Node로 재설정
       this.first = newNode;
@@ -76,7 +75,7 @@ class Stack {
 class Node {
   value: any;
   next: Node;
-  
+
   constructor(value) {
     this.value = value;
     this.next = null;
