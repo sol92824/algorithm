7958a1df42530b0ef528e25adc03aeab8f81eee3
sol92824 <sol92824@gmail.com>
2024-03-24 15:02:10 +0900

    queue : 배열로 큐 만들기

--- src/queue/queue.controller.ts
+++ src/queue/queue.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { QueueService } from './queue.service';
 
 @Controller('queue')
 export class QueueController {
   constructor(private readonly queueService: QueueService) {}
+
+  @Get('queue-made-with-array')
+  queueMadeWithArray(): void {
+    return this.queueService.queueMadeWithArray();
+  }
 }
--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class QueueService {}
+export class QueueService {
+  queueMadeWithArray() {
+    const queue = [];
+
+    // 입력값을 push로 배열의 마지막에 추가
+    queue.push('FIRST');
+    queue.push('SECOND');
+    queue.push('THIRD');
+
+    console.log(queue);
+
+    // 출력값을 shift로 배열의 처음에서 제거
+    console.log(queue.shift());
+    console.log(queue.shift());
+    console.log(queue.shift());
+
+    const queue2 = [];
+
+    // 입력값을 unshift로 배열의 처음에 추가
+    queue2.unshift('FIRST');
+    queue2.unshift('SECOND');
+    queue2.unshift('THIRD');
+
+    console.log(queue2);
+
+    // 출력값을 pop으로 배열의 마지막에서 제거
+    console.log(queue2.pop());
+    console.log(queue2.pop());
+    console.log(queue2.pop());
+  }
+}
