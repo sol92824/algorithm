add58584c45c0e86f7215f4cd81268ff7b211d7f
sol92824 <sol92824@gmail.com>
2024-03-30 11:23:53 +0900

    The Wild West 59 주석 추가

--- src/binary-search-tree/binary-search-tree.service.ts
+++ src/binary-search-tree/binary-search-tree.service.ts
@@ -42,25 +42,34 @@ class BinarySearchTree {
 
     let current = this.root;
 
+    // 3. 반복문 실행
     while (true) {
+      // 4. 이미 트리에 존재하는 값을 입력하면 undefined 반환
       if (current.value === value) {
         return undefined;
       }
 
+      // 5. 입력된 값이 현재 위치의 값보다 작으면 왼쪽
       if (current.value > value) {
+        // 5-1. 왼쪽이 비어있으면 새로운 노드로 채우기
         if (current.left === null) {
           current.left = newNode;
           return this;
         }
 
+        // 5-2. 비어있지 않다면 현재 위치 왼쪽 노드로 이동
         current = current.left;
-      } else {
+      }
+      // 6. 입력된 값이 현재 위치의 값보다 크면 오른쪽
+      else {
+        // 6-1. 오른쪽이 비어있으면 새로운 노드로 채우기
         if (current.right === null) {
           current.right = newNode;
 
           return this;
         }
 
+        // 6-2. 비어있지 않다면 현재 위치 오른쪽 노드로 이동
         current = current.right;
       }
     }
