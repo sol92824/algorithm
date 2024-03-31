7236d084afa1bfe939b6df6e34f3d84f0aa05bae
sol92824 <sol92824@gmail.com>
2024-04-01 03:20:28 +0900

    hashTable.set()

--- src/hash-tables/hash-tables.controller.ts
+++ src/hash-tables/hash-tables.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HashTablesService } from './hash-tables.service';
 
 @Controller('hash-tables')
 export class HashTablesController {
   constructor(private readonly hashTablesService: HashTablesService) {}
+
+  @Get('hashTable')
+  hashTable(): void {
+    return this.hashTablesService.hashTable();
+  }
 }
--- src/hash-tables/hash-tables.service.ts
+++ src/hash-tables/hash-tables.service.ts
@@ -26,4 +26,50 @@ export class HashTablesService {
 
     return total;
   }
+
+  hashTable() {
+    const ht = new HashTable();
+    console.log(ht.set('hello world', 'goodbye!!'));
+    console.log(ht.set('dogs', 'are cool'));
+    console.log(ht.set('cats', 'are fine'));
+    console.log(ht.set('i love', 'pizza'));
+    console.log(ht.set('hi', 'bye'));
+    console.log(ht.set('french', 'fries'));
+    console.log(ht.keyMap);
+  }
+}
+
+class HashTable {
+  keyMap: any[];
+
+  constructor(size = 4) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key: string) {
+    let total = 0;
+    const WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const char = key[i];
+      const value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
+
+  set(key: string, value: any) {
+    // 1. _hash 함수를 통해 keyMap에 담을 인덱스 위치 받아오기
+    const index = this._hash(key);
+
+    // 2. 만약 keyMap[index]에 해당하는 위치에 값이 없었다면 빈 배열 우선 생성
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+
+    // 3. keyMap[index]의 값인 배열에 현재 입력된 데이터 넣어주기
+    this.keyMap[index].push([key, value]);
+
+    return index;
+  }
 }
